f05c05ec3813253644cfe092e9eb37e152e6fff0
8ft0 <130460431+8ft0-ai@users.noreply.github.com>
2026-09-19 13:25:54 +1000

    Align Wikipedia source URL expectation

--- components/country-panel.test.tsx
+++ components/country-panel.test.tsx
@@ -186,7 +186,7 @@ describe('CountryPanel', () => {
       }),
     ).toHaveAttribute(
       'href',
-      'https://en.wikipedia.org/wiki/National_People%27s_Congress',
+      'https://en.wikipedia.org/wiki/National_People's_Congress',
     );
   });
 
